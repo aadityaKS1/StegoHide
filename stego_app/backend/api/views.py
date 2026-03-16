004ad36f4cadfb07afbf312fcc7946531ef06e95
Aaditya Kumar Singh <aaditya.creates@gmail.com>
2026-03-16 16:09:08 +0545

    v2

--- stego_app/backend/api/views.py
+++ stego_app/backend/api/views.py
@@ -3,38 +3,50 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from PIL import Image
 import base64
+import Crypto.Hash.SHA256 as SHA256
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import AES
 
-
-# Create your views here.
-#converting password to key
-def password_to_key(password):
-    key=0
-    for char in password:
-        key+=ord(char)
-    return key % 256
-
-
-
-#encrypting the message using XOR operation with a key
-def encrypt_message(message,key):
-    encrypted_message=[]
-    for  char in message:
-        encrypted=ord(char)^key
-        encrypted_message.append(encrypted)
-    message_len=len(encrypted_message)
-    return encrypted_message,message_len
-
-#decrypting the message using XOR operation with a passord-derived key
-def decrypt_message(encrypted_message, key):
-    message = ""
+#converitng encrypted message to binary
+def to_binary(encrypted_message):
+    binary=""
     for num in encrypted_message:
-        char = chr(num ^ key)
-        message += char
-    return message 
+        binary+=format(num,'08b')
+    return binary
+
+#converting password to key using SHA256 hashing algorithm
+def password_to_key(password):
+    password=str(password)
+    return SHA256.new(password.encode()).digest()
+
+#encrypting the message using AES encryption algorithm in CBC mode
+def encrypt_message_AES(message, password):
+    key = password_to_key(password)
+    cipher=AES.new(key, AES.MODE_CBC) 
+    ciphertext = cipher.encrypt(pad(message.encode(),16))
+    iv = cipher.iv
+    full_cipher = iv + ciphertext
+    return list(full_cipher), len(full_cipher) # return list of integers and length of the ciphertext easy to store in image
+
+
+#decrypting the message using AES decryption algorithm in CBC mode
+def decrypt_message_AES(encrypted_message, password):
+    key = password_to_key(password)
+    encrypted_bytes = bytes(encrypted_message)
+    iv = encrypted_bytes[:16]
+    ciphertext = encrypted_bytes[16:]
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    decrypted = unpad(cipher.decrypt(ciphertext),16)
+    return decrypted.decode()
 
 # extract first 32 bits (message length)
 def get_message_length(img):
+    capacity = img.width * img.height * 3
 
+    if len(payload) > capacity:
+        return Response({
+            "error": "Message too large for this image"
+        })
     bits = ""
 
     for y in range(img.height):
@@ -48,13 +60,37 @@ def get_message_length(img):
 
             if len(bits) >= 32:
                 return int(bits[:32],2)
-            
-#converitng encrypted message to binary
-def to_binary(encrypted_message):
-    binary=""
-    for num in encrypted_message:
-        binary+=format(num,'08b')
-    return binary
+    return 0
+#embedding the encrypted message into the image using least significant bit (LSB) steganography
+def embed_payload(img,payload):
+
+    pixels = img.load()
+
+    bit_index = 0
+
+    for y in range(img.height):
+        for x in range(img.width):
+
+            if bit_index >= len(payload):
+                return img
+
+            r,g,b = pixels[x,y]
+
+            if bit_index < len(payload):
+                r = (r & ~1) | int(payload[bit_index])
+                bit_index += 1
+
+            if bit_index < len(payload):
+                g = (g & ~1) | int(payload[bit_index])
+                bit_index += 1
+
+            if bit_index < len(payload):
+                b = (b & ~1) | int(payload[bit_index])
+                bit_index += 1
+
+            pixels[x,y] = (r,g,b)
+
+    return img
 
 #extracting the encrypted message from the image
 def extract_message(enc_img, message_len):
@@ -96,9 +132,8 @@ def encode_image(request):
     message=request.data['message']
     password=request.data['password']  
     
-    img=Image.open(image)
-    key=password_to_key(password)
-    encrypted_message,message_len=encrypt_message(message,key)
+    img=Image.open(image).convert("RGB")
+    encrypted_message,message_len=encrypt_message_AES(message,password)
     binary_message=to_binary(encrypted_message)
     length_bits = format(message_len,'032b')
     payload = length_bits + binary_message
@@ -128,9 +163,15 @@ def encode_image(request):
         if bit_index>=len(payload):
             break
     
-    img.save("encoded.png")
+    import os
+
+    filename = image.name
+    name, ext = os.path.splitext(filename)
+    new_filename = f"{name}_encoded{ext}"
+
+    img.save(new_filename,format="PNG")
     
-    with open("encoded.png","rb") as f:
+    with open(new_filename,"rb") as f:
         encoded = base64.b64encode(f.read()).decode()
     return Response({"encoded_image":encoded})
 
@@ -140,7 +181,7 @@ def decode_message(request):
     image = request.FILES['image']
     password = request.data['password']
 
-    img = Image.open(image)
+    img = Image.open(image).convert("RGB")
 
     # STEP 1: get message length
     message_len = get_message_length(img)
@@ -149,8 +190,7 @@ def decode_message(request):
     encrypted_numbers = extract_message(img, message_len)
 
     # STEP 3: decrypt message
-    key = password_to_key(password)
-    message = decrypt_message(encrypted_numbers, key)
+    message = decrypt_message_AES(encrypted_numbers, password)
 
     return Response({
         "decoded_message": message
